fa9f76664c1cbe2e4ac8d06e52b9369a662d238b
gnsc4 <gnschris@hotmail.com>
2025-03-06 13:54:13 -0500

    added MetaHeaders to see if this is the issue

--- TestGMPDA.raw.user.js
+++ TestGMPDA.raw.user.js
@@ -1,6 +1,14 @@
 // ==UserScript==
-// @name         Minimal Test Script - Alert PDA
+// @name         Minimal Test Script - Alert PDA (MetaHeaders)
+// @namespace    torn.minimaltest.alertpda
+// @version      0.1
+// @description  Minimal test script for Torn PDA with alert and metadata headers
+// @author       GNSC4
 // @match        https://www.torn.com/loader.php?sid=racing*
+// @grant        none
+// @updateURL    https://github.com/gnsc4/Torn-Scripts/raw/refs/heads/master/TestGMPDA.raw.user.js
+// @downloadURL  https://github.com/gnsc4/Torn-Scripts/raw/refs/heads/master/TestGMPDA.raw.user.js
+// @license      MIT
 // ==/UserScript==
 
-alert('Minimal PDA Alert Script Loaded!');
+alert('Minimal PDA Alert Script Loaded! (MetaHeaders)');
