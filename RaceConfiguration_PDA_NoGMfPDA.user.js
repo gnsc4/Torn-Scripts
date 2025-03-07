bd1138cd4c653bbeb72f81b4ff134e623339baa4
gnsc4 <gnschris@hotmail.com>
2025-03-06 21:45:02 -0500

    update urls

--- RaceConfiguration_PDA_NoGMfPDA.user.js
+++ RaceConfiguration_PDA_NoGMfPDA.user.js
@@ -3,8 +3,8 @@
 // @namespace    torn.raceconfiggui.pdadesktop
 // @description  Full Feature Race Config GUI - v2.85 - User Enlisted Cars Endpoint - Official API Domain - ROBUST CACHED DATA + "UPDATE CARS" BUTTON - DATA OPTIMIZATION (item_name, id only) - PARAMETER CONFIRMATION (Race Creation Params) - **ERROR & SYNTAX FIX - GUI Cleanup - RESTORED RACE BUTTON**
 // @version      2.85-PDA-Desktop-GMfPDA-FullGUI-UserEnlistedCars-OfficialAPI-ROBUST-CACHED-DATA-UPDATE-CARS-BUTTON-DATA-OPTIMIZED-PARAM-CONFIRM-ERROR-SYNTAX-GUI-CLEANUP-RACE-BUTTON
-// @updateURL    https://github.com/gnsc4/Torn-Scripts/raw/refs/heads/master/RaceConfiguration_PDA_NoGMfPDA.user.js  // <-- IMPORTANT: Use REAL URL
-// @downloadURL  https://github.com/gnsc4/Torn-Scripts/raw/refs/heads/master/RaceConfiguration_PDA_NoGMfPDA.user.js // <-- IMPORTANT: Use REAL URL
+// @updateURL    https://github.com/gnsc4/Torn-Scripts/raw/refs/heads/master/RaceConfiguration_PDA_NoGMfPDA.user.js
+// @downloadURL  https://github.com/gnsc4/Torn-Scripts/raw/refs/heads/master/RaceConfiguration_PDA_NoGMfPDA.user.js
 // @author       GNSC4 [268863] (Based on Shlefter's script)
 // @match        https://www.torn.com/loader.php?sid=racing*
 // @grant        GM_setValue
